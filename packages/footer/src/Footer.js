9adee6f130499fa12c790f34f555bbc6a36d8652
Frank Desmettre <frank.desmettre@gmail.com>
2018-04-16 16:31:28 +0200

    Add airbnb, fix footer

--- packages/footer/src/Footer.js
+++ packages/footer/src/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Footer = () => {
+const Footer = ({ style }) => {
   return (
-    <footer className="o-footer">
+    <footer className="o-footer" style={style}>
       <span className="o-footer__label">© 2017 Stad Antwerpen</span>
       <a href="/" className="o-footer__button a-button a-button--secondary has-icon">
         <i className="fa fa-arrow-up"></i>
