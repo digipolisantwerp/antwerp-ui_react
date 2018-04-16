0a6735533524f88a0c4f0c455bfedd178a1fea1a
stefan <me@stefanpante.com>
2018-04-16 15:41:49 +0200

    added styleguide.config.js

---
+++ styleguide.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  components: 'packages/*/src/!(index).js',
+  webpackConfig: {
+    module: {
+      rules: [
+        // Babel loader, will use your project’s .babelrc
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    }
+  }
+};
