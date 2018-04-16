9c65a5e87bf902837ce593263a86c40420360406
Tom Dilliën <tom@pau.be>
2018-04-16 16:29:57 +0200

    Component to React

--- packages/taglist/src/TagListItem/TagListItem.js
+++ packages/taglist/src/TagListItem/TagListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../../../button';
 
-class TagListItem extends Component.Component {
+class TagListItem extends React.Component {
     render() {
         const {
             onClick,
