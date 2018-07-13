0fc5d20252434157d1fa8bbef18058e91a834615
Tom Ceuppens <t-c@live.be>
2018-07-13 16:52:11 +0200

    tst fix

--- packages/alert/src/Alert.spec.js
+++ packages/alert/src/Alert.spec.js
@@ -2,19 +2,24 @@ import Alert from './Alert';
 import { mount, shallow } from 'enzyme';
 import React from 'react';
 
-test('should apply the correct className - danger', () => {
+test('should apply the correct className for type - danger', () => {
     const alert = mount(<Alert type="danger">test</Alert>);
     expect(alert.getDOMNode().className).toContain("danger");
 });
 
-test('should apply the correct className - success', () => {
+test('should apply the correct className for type - success', () => {
     const alert = mount(<Alert type="success">test</Alert>);
     expect(alert.getDOMNode().className).toContain("success");
 });
 
-test('should apply the correct className - warning', () => {
+test('should apply the correct className for type - warning', () => {
     const alert = mount(<Alert type="warning">test</Alert>);
     expect(alert.getDOMNode().className).toContain("warning");
+});
+
+test('should apply the correct className', () => {
+    const alert = mount(<Alert className="test">test</Alert>);
+    expect(alert.getDOMNode().className).toContain("test");
 });
 
 test('should set the title', () => {
