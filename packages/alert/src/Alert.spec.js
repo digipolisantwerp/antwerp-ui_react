cbb3109fde6dd92ba8989a9e5eaecc83ad19c5de
Tom Ceuppens <t-c@live.be>
2018-07-16 15:34:57 +0200

    button spec + package.json fixes

--- packages/alert/src/Alert.spec.js
+++ packages/alert/src/Alert.spec.js
@@ -2,39 +2,41 @@ import Alert from './Alert';
 import { mount, shallow } from 'enzyme';
 import React from 'react';
 
-test('should apply the correct className for type - danger', () => {
-    const alert = mount(<Alert type="danger">test</Alert>);
-    expect(alert.getDOMNode().className).toContain("danger");
-});
+describe('Alert', () => {
+    it('should apply the correct className for type - danger', () => {
+        const alert = mount(<Alert type="danger">test</Alert>);
+        expect(alert.getDOMNode().className).toContain("danger");
+    });
 
-test('should apply the correct className for type - success', () => {
-    const alert = mount(<Alert type="success">test</Alert>);
-    expect(alert.getDOMNode().className).toContain("success");
-});
+    it('should apply the correct className for type - success', () => {
+        const alert = mount(<Alert type="success">test</Alert>);
+        expect(alert.getDOMNode().className).toContain("success");
+    });
 
-test('should apply the correct className for type - warning', () => {
-    const alert = mount(<Alert type="warning">test</Alert>);
-    expect(alert.getDOMNode().className).toContain("warning");
-});
+    it('should apply the correct className for type - warning', () => {
+        const alert = mount(<Alert type="warning">test</Alert>);
+        expect(alert.getDOMNode().className).toContain("warning");
+    });
 
-test('should apply the correct className', () => {
-    const alert = mount(<Alert className="test">test</Alert>);
-    expect(alert.getDOMNode().className).toContain("test");
-});
+    it('should apply the correct className', () => {
+        const alert = mount(<Alert className="test">test</Alert>);
+        expect(alert.getDOMNode().className).toContain("test");
+    });
 
-test('should set the title', () => {
-    const alert = shallow(<Alert title="test">Test</Alert>);
-    expect(alert.find('h5').text()).toBe('test');
-});
+    it('should set the title', () => {
+        const alert = shallow(<Alert title="test">Test</Alert>);
+        expect(alert.find('h5').text()).toBe('test');
+    });
 
-test('should set type on the close button', () => {
-    const alert = shallow(<Alert type="warning" closable={true}>Test</Alert>);
-    expect(alert.find('Button').props().type).toBe('warning');
-});
+    it('should set type on the close button', () => {
+        const alert = shallow(<Alert type="warning" closable={true}>Test</Alert>);
+        expect(alert.find('Button').props().type).toBe('warning');
+    });
 
-test('should call the preventDefault()', () => {
-    const alert = shallow(<Alert type="warning" closable={true}>Test</Alert>);
-    const cbMock = jest.fn();
-    alert.find('Button').simulate('click', { preventDefault: cbMock });
-    expect(cbMock).toBeCalled();
+    it('should call the preventDefault()', () => {
+        const alert = shallow(<Alert type="warning" closable={true}>Test</Alert>);
+        const cbMock = jest.fn();
+        alert.find('Button').simulate('click', { preventDefault: cbMock });
+        expect(cbMock).toBeCalled();
+    });
 });
--- packages/avatar/src/Avatar.spec.js
+++ packages/avatar/src/Avatar.spec.js
@@ -2,39 +2,41 @@ import Avatar from './Avatar';
 import { mount } from 'enzyme';
 import React from 'react';
 
-test('should apply the correct className for size - small', () => {
-    const avatar = mount(<Avatar size="small">test</Avatar>);
-    expect(avatar.getDOMNode().className).toContain("a-avatar--small");
-});
+describe('Avatar', () => {
+    it('should apply the correct className for size - small', () => {
+        const avatar = mount(<Avatar size="small">test</Avatar>);
+        expect(avatar.getDOMNode().className).toContain("a-avatar--small");
+    });
 
-test('should apply the correct className for size - medium', () => {
-    const avatar = mount(<Avatar size="medium">test</Avatar>);
-    expect(avatar.getDOMNode().className).toContain("a-avatar--medium");
-});
+    it('should apply the correct className for size - medium', () => {
+        const avatar = mount(<Avatar size="medium">test</Avatar>);
+        expect(avatar.getDOMNode().className).toContain("a-avatar--medium");
+    });
 
-test('should apply the correct className for size - large', () => {
-    const avatar = mount(<Avatar size="large">test</Avatar>);
-    expect(avatar.getDOMNode().className).toContain("a-avatar--large");
-});
+    it('should apply the correct className for size - large', () => {
+        const avatar = mount(<Avatar size="large">test</Avatar>);
+        expect(avatar.getDOMNode().className).toContain("a-avatar--large");
+    });
 
-test('it should apply the style object', () => {
-    const avatarStyle = { color: 'green' };
-    const avatar = mount(<Avatar style={avatarStyle} />);
-    expect(avatar.getDOMNode().style).toHaveProperty('color');
-});
+    it('should apply the style object', () => {
+        const avatarStyle = { color: 'green' };
+        const avatar = mount(<Avatar style={avatarStyle} />);
+        expect(avatar.getDOMNode().style).toHaveProperty('color');
+    });
 
-test('it should show the icon when provided', () => {
-    const avatar = mount(<Avatar icon="bell" />);
-    expect(avatar.find('i').props().className).toContain('fa-bell');
-});
+    it('should show the icon when provided', () => {
+        const avatar = mount(<Avatar icon="bell" />);
+        expect(avatar.find('i').props().className).toContain('fa-bell');
+    });
 
-test('it should show the letters when provided', () => {
-    const avatar = mount(<Avatar letter="SP" />);
-    expect(avatar.find('span').text()).toBe('SP');
-});
+    it('should show the letters when provided', () => {
+        const avatar = mount(<Avatar letter="SP" />);
+        expect(avatar.find('span').text()).toBe('SP');
+    });
 
-test('it should show the image and alt text when provided', () => {
-    const avatar = mount(<Avatar image="http://www.google.be" alt="google" />);
-    expect(avatar.find('img').props().src).toBe('http://www.google.be');
-    expect(avatar.find('img').props().alt).toBe('google');
+    it('should show the image and alt text when provided', () => {
+        const avatar = mount(<Avatar image="http://www.google.be" alt="google" />);
+        expect(avatar.find('img').props().src).toBe('http://www.google.be');
+        expect(avatar.find('img').props().alt).toBe('google');
+    });
 });
