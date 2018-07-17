dbc7abd3c4451baada06284e531cdfbcb836ef71
Tom Ceuppens <t-c@live.be>
2018-07-17 10:36:31 +0200

    footer spec

---
+++ packages/footer/src/Footer.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from './Footer';
+
+describe('Footer', () => {
+    it('should apply the style', () => {
+        const footerStyle = { color: 'green'};
+        const footer = mount(<Footer style={footerStyle}/>);
+        expect(footer.getDOMNode().style).toHaveProperty('color');
+    });
+
+    it('should apply the children to the footer label', () => {
+        const footer = mount(<Footer>test</Footer>);
+        expect(footer.find('.o-footer__label').text()).toBe('test');
+    });
+
+    it('should return in a footer element', () => {
+        const footer = mount(<Footer />);
+        expect(footer.getDOMNode().tagName).toBe('FOOTER');
+    });
+});
