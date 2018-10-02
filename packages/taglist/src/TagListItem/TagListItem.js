e7097951eebc80b194a979e1ebc4a333410505d9
Frank Desmettre <frank.desmettre@gmail.com>
2018-10-02 17:15:00 +0200

    Transparent TagListItem fix

--- packages/taglist/src/TagListItem/TagListItem.js
+++ packages/taglist/src/TagListItem/TagListItem.js
@@ -2,44 +2,41 @@ import React, { Component } from 'react';
 import Button from '../../../button/src/Button';
 
 type Props = {
-	/** The label to display. */
-	value: string,
-	/** Make the tag closable or not. */
-	closable?: boolean,
-	/** Add a custom icon like for example 'check'. */
-	icon?: string,
-	/** Add a custom buttonclass like for example 'success'. */
-	buttonClass?: string,
-	/** Add a custom function that gets triggered when the tag gets closes. */
-	onClick?: void,
+  /** The label to display. */
+  value: string,
+  /** Make the tag closable or not. */
+  closable?: boolean,
+  /** Add a custom icon like for example 'check'. */
+  icon?: string,
+  /** Add a custom buttonclass like for example 'success'. */
+  buttonClass?: string,
+  /** Add a custom function that gets triggered when the tag gets closes. */
+  onClick?: void,
 };
 
 class TagListItem extends Component<Props> {
-    render() {
-        const {
-            value,
-            closable = false,
-            icon = '',
-            buttonClass = 'a-button--default',
-					  onClick,
+  render() {
+    const {
+      value,
+      closable = false,
+      icon = '',
+      buttonClass = 'a-button--default',
+      onClick,
 
-        } = this.props;
+    } = this.props;
 
-        return (
-            <li className="m-tag">
-                {icon &&
-                    <Button className={'a-button a-button--small has-icon ' + (buttonClass && 'a-button--' + buttonClass)} icon={icon}></Button>
-                }
-                <span className="m-tag__label">{value}</span>
-                {closable &&
-                    <Button className="a-button--transparent a-button--small a-button--danger has-icon"
-                            onClick={onClick}
-                            icon="close">
-                    </Button>
-                }
-            </li>
-        )
-    }
+    return (
+      <li className="m-tag">
+        {icon &&
+          <Button size="small" icon={icon}></Button>
+        }
+        <span className="m-tag__label">{value}</span>
+        {closable &&
+          <Button transparent size="small" type="danger" icon="close" onClick={onClick} />
+        }
+      </li>
+    )
+  }
 }
 
 export default TagListItem;
