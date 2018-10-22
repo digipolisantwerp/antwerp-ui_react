fed19b3aa17ad8130b1256ad068ed3aa3d840b37
Tom Dilliën <tom@pau.be>
2018-10-22 10:25:00 +0200

    Commit bundle.js and index.html for styleguidist

--- packages/switch/src/Switch.js
+++ packages/switch/src/Switch.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import classNames from 'classnames';
 import InputLabel from '../../form/src/InputLabel';
 
@@ -7,66 +7,66 @@ class Switch extends Component {
 		checked: this.props.checked || false
 	}
 
-  static getDerivedStateFromProps(nextProps, prevState){
-    if (nextProps.checked !== prevState.checked) {
-      return {
-        checked: nextProps.checked
-      };
-    }
+	static getDerivedStateFromProps(nextProps, prevState) {
+		if (nextProps.checked !== prevState.checked) {
+			return {
+				checked: nextProps.checked
+			};
+		}
 
-    return null;
-  }
+		return null;
+	}
 
 	onClick = (e) => {
-    this.setState({ checked: !this.state.checked });
+		this.setState({checked: !this.state.checked});
 
-    if (this.props.onClick) {
-      this.props.onClick(e);
-    }
-  }
+		if (this.props.onClick) {
+			this.props.onClick(e);
+		}
+	}
 
-  render() {
-    const {
-      id,
-      label,
-      name,
-      labelTrue,
-      labelFalse,
-      required,
-      disabled,
-    } = this.props;
+	render() {
+		const {
+			id,
+			label,
+			name,
+			labelTrue,
+			labelFalse,
+			required,
+			disabled,
+		} = this.props;
 
-    const containerClass = classNames(
-      'a-input',
-      { 'is-required': !!required }
-    );
+		const containerClass = classNames(
+			'a-input',
+			{'is-required': !!required}
+		);
 
-    const switchWrapperClass = classNames(
-      'a-switch',
-      { 'disabled': disabled }
-    );
+		const switchWrapperClass = classNames(
+			'a-switch',
+			{'disabled': disabled}
+		);
 
-    return (
-      <div className={containerClass}>
-        {label && <InputLabel htmlFor={id}>{label}</InputLabel>}
-        <div className={switchWrapperClass}>
-          <label className="a-switch__label">{labelFalse ? labelFalse : 'Nee'}</label>
-          <div className="a-switch__toggle">
-            <input
-               type="checkbox"
-               name={name}
-               id={id}
-               checked={this.state.checked}
-               disabled={disabled}
-               onClick={this.onClick}
-            />
-            <label htmlFor={id}></label>
-          </div>
-          <label className="a-switch__label">{labelTrue ? labelTrue : 'Ja'}</label>
-        </div>
-      </div>
-    )
-  }
+		return (
+			<div className={containerClass}>
+				{label && <InputLabel htmlFor={id}>{label}</InputLabel>}
+				<div className={switchWrapperClass}>
+					<label className="a-switch__label">{labelFalse ? labelFalse : 'Nee'}</label>
+					<div className="a-switch__toggle">
+						<input
+							type="checkbox"
+							name={name}
+							id={id}
+							checked={this.state.checked}
+							disabled={disabled}
+							onClick={this.onClick}
+						/>
+						<label htmlFor={id}></label>
+					</div>
+					<label className="a-switch__label">{labelTrue ? labelTrue : 'Ja'}</label>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Switch;
