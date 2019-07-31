fbf2123657dd35b67d8c9f064b659b3fce4b4dcb
Tom Dilliën <tom@pau.be>
2019-07-31 11:06:13 +0200

    Add type props

--- packages/subfooter/src/SubFooter.js
+++ packages/subfooter/src/SubFooter.js
@@ -3,7 +3,13 @@ import StandardSubFooter from './Blocks/StandardSubFooter';
 
 import './Subfooter.scss';
 
-const SubFooter = ({ type, children }) => {
+type Props = {
+  /** Get standard subfooter such as for type 'antwerpen' */
+  type?: string,
+  children?: any,
+}
+
+const SubFooter = ({ type, children }: Props) => {
   if (type) {
     return <StandardSubFooter type={type} />;
   }
