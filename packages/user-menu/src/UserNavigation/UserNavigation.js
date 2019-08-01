f76bc7bf2182ecdc0b3f166e8a3b922bb8b53776
Tom Dilliën <tom@pau.be>
2019-08-01 16:56:20 +0200

    Change Notifications text + rendering

--- packages/user-menu/src/UserNavigation/UserNavigation.js
+++ packages/user-menu/src/UserNavigation/UserNavigation.js
@@ -10,27 +10,19 @@ import {
   myEloketUrl,
 } from './UserNavigationUrls';
 
-function renderNotificationsMenuItem(notificationsCount) {
-  if (notificationsCount) {
-    return (
-      <li>
-        <a href={notificationsUrl()} title="Notificaties" alt="Klikt hier om uw notificaties te bekijken.">
-          U heeft notificaties:
-          <span className="badge">43</span>
-        </a>
-      </li>
-    )
-  } else {
-    return <li><a href={notificationsUrl()} title="Notificaties" alt="Klikt hier om uw notificaties te bekijken.">Notificaties</a></li>
-  }
-}
-
 const UserNavigation = (props) => {
   const { children, notificationsCount = '' } = props;
 
   return (
     <ul className="m-nav-list">
-      {renderNotificationsMenuItem(notificationsCount)}
+      <li>
+        <a href={notificationsUrl()} title="Notificaties" alt="Klikt hier om uw notificaties te bekijken.">
+          Notificaties
+          {notificationsCount &&
+            <span className="badge">{notificationsCount}</span>
+          }
+        </a>
+      </li>
       <li><a href={aProfileUrl()} title="A-profiel beheren" alt="Hier krijgt u toegang tot uw A-profiel en uw persoonlijke instellingen.">A-profiel beheren</a></li>
       <li><a href={favoritesUrl()} title="Favorieten" alt="Klik hier om uw favorieten te bekijken.">Favorieten</a></li>
       <li><a href={linkingUrl()} title="Koppelingen" alt="Klik hier om uw eID, A-kaart en meer te koppelen.">Koppelingen</a></li>
