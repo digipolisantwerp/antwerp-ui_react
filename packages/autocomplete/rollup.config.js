e456a3c59c73380a6a6969443fa71f1226856bae
Michael Castiau <castiau.m@gmail.com>
2020-05-05 11:28:38 +0200

    Fix build

--- packages/autocomplete/rollup.config.js
+++ packages/autocomplete/rollup.config.js
@@ -1,20 +1,18 @@
-
-
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.main, format: 'cjs' },
-		{ file: pkg.module, format: 'es' }
-	],
-	plugins: [
-		resolve(),
-		babel({
-			exclude: ['node_modules/**']
-		})
-	],
-	external: ['react', 'react-dom', 'classnames', 'react-input-mask'],
+  input: 'src/index.js',
+  output: [
+    {file: pkg.main, format: 'cjs'},
+    {file: pkg.module, format: 'es'}
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: ['node_modules/**']
+    })
+  ],
+  external: ['react', 'react-dom', 'classnames', 'react-reactive-form', 'react-input-mask'],
 };
--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,17 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default {
-	input: 'index.js',
-	output: [
-		{ file: pkg.main, format: 'cjs' },
-		{ file: pkg.module, format: 'es' }
-	],
-	plugins: [
-		resolve(),
-		babel({
-			exclude: ['node_modules/**'],
+  input: 'index.js',
+  output: [
+    {file: pkg.main, format: 'cjs'},
+    {file: pkg.module, format: 'es'}
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: ['node_modules/**'],
       plugins: ['external-helpers']
-		})
-	],
-	external: ['react', 'classnames', 'moment', 'react-dom', 'react-input-mask', 'react-modal'],
+    })
+  ],
+  external: ['react', 'classnames', 'moment', 'react-reactive-form', 'react-dom', 'react-input-mask', 'react-modal'],
 };
