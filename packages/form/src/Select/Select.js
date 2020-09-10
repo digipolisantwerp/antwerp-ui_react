3d89b422b10f6c714645c45fba1ddfced6360a95
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2020-09-10 10:50:36 +0200

    feat(select): add disabled state

--- packages/form/src/Select/Select.js
+++ packages/form/src/Select/Select.js
@@ -26,6 +26,7 @@ type Props = {
   options?: Array<SelectOption>,
   onChange?: (e: object) => void,
   value?: string,
+  disabled?: boolean,
   type?: InputTypes,
   /** Qa id */
   qa?: string,
@@ -56,6 +57,7 @@ class Select extends Component<Props> {
       onChange,
       value,
       loading,
+      boolean,
       qa,
       type,
     } = this.props;
@@ -80,7 +82,7 @@ class Select extends Component<Props> {
       <div className={selectClass} name={name} data-qa={qa}>
         {label && <InputLabel inline={inline} htmlFor={id}>{label}</InputLabel>}
         <div className={wrapperClass}>
-          <select id={id} name={name} onChange={(e) => this.onChange(e)} value={value} defaultValue={placeholder}>
+          <select id={id} name={name} onChange={(e) => this.onChange(e)} value={value} defaultValue={placeholder} disabled={disabled}>
             {placeholder && <option value={placeholder} disabled>{placeholder}</option>}
             {options.map(option => (
               <Option
