84677f6a5ac8d7232ea30b420fba32ec9480a73a
Laurens Lavaert <llavaert@gmail.com>
2020-09-16 11:20:54 +0200

    feat: Allow accordion tab to manage state in parent

--- packages/accordion/src/AccordionTab/AccordionTab.js
+++ packages/accordion/src/AccordionTab/AccordionTab.js
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 type Props = {
   children: React.node,
   open?: boolean,
+  toggle?: (e: React.MouseEvent<HTMLDivElement>) => void;
 };
 
 class AccordionTab extends React.Component<Props> {
@@ -22,7 +23,8 @@ class AccordionTab extends React.Component<Props> {
     }
   }
 
-  toggle() {
+  toggle(e) {
+    if (this.props.toggle) return this.props.toggle(e);
     const { isOpen } = this.state;
     this.setState({
       isOpen: !isOpen,
@@ -38,8 +40,8 @@ class AccordionTab extends React.Component<Props> {
         role="button"
         tabIndex="0"
         data-qa={qa}
-        onKeyPress={() => this.toggle()}
-        onClick={() => this.toggle()}
+        onKeyPress={(e) => this.toggle(e)}
+        onClick={(e) => this.toggle(e)}
         className={classNames('m-accordion__tab', { 'is-open': isOpen })}
       >
         { children }
